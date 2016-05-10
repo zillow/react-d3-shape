893e8d28a2e24407e4fb320a41713aecdda7e0a3
Rob McGuire-Dale <robmc@zillow.com>
2016-05-09 17:39:36 -0700

    Add alignment accessor function

--- src/utils/alignment.jsx
+++ src/utils/alignment.jsx
@@ -15,3 +15,30 @@ export function getTranslateXAmount (pointAlign, bandwidth) {
   }
   return 0;
 }
+
+/**
+ * Create an X accessor function to help create a D3 line or area such that the
+ * leftmost shape is left-aligned, and the rightmost is right-aligned.
+ */
+export function getXPointAlignAccessor (data, pointAlign, xScaleSet) {
+  return (d, i) => {
+
+    // First element, always left-align
+    if (i === 0) {
+      console.log('1st, left');
+      return xScaleSet(d.x) +
+        getTranslateXAmount('left', xScaleSet.bandwidth());
+
+    // Last element, always right-align
+    } else if (i === data.length - 1) {
+      console.log('last, right');
+      return xScaleSet(d.x) +
+        getTranslateXAmount('right', xScaleSet.bandwidth());
+    }
+
+    // Other elements, align as specified
+    console.log('other, normal');
+    return xScaleSet(d.x) +
+      getTranslateXAmount(pointAlign, xScaleSet.bandwidth())
+  }
+}
